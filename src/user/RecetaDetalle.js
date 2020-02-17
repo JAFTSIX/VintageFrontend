cfac4bce9ea9c6916e3d7a8fb025f6604950d314
Rebeca <50533726+Eceber@users.noreply.github.com>
2020-02-16 23:02:37 -0600

    Create RecetaDetalle.js

---
+++ src/user/RecetaDetalle.js
@@ -0,0 +1,31 @@
+import React, {useState, useEffect} from 'react';
+import Layout from '../nucleo/Layout';
+import {getReceta} from './apiReceta';
+import RecetaInterfaz from './RecetaInterfaz';
+
+const RecetaDetalle = (props) => {
+    const [receta, setReceta] = useState({});
+    const [error, setError] = useState(false);
+
+    const cargarDetalleReceta = recetaId => {
+
+    }
+
+    //cargar todo apenas entre a la pagina y cada vez que se cambie el state
+    useEffect(() => {
+        //guardar el id de la receta del url
+        const recetaId = props.match.params.recetaId
+    }, []);
+
+    return(
+        <Layout 
+            titulo="Detalle Receta"
+            className="container-fluid"
+        >
+            <p>detalle receta</p>
+            
+        </Layout>
+    );
+}
+
+export default RecetaDetalle;
