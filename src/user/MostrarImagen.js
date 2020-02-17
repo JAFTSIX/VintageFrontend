7a280b6d5b70f5e23f0017038d4035663475a70c
Rebeca <50533726+Eceber@users.noreply.github.com>
2020-02-16 19:47:19 -0600

    Update MostrarImagen.js

--- src/user/MostrarImagen.js
+++ src/user/MostrarImagen.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import {crearProducto} from '../admin/apiAdmin';
 import {API} from '../config';
 
 //item = producto
 //url para hacer request al api
-const MostrarImagen = ({item, url}) => (
+const MostrarImagen = ({item, url}) => {
+    
+    return(
+
     <div className="productoImagen">
         {/* para ver cual es el url se va al api  VIDEO 84*/}
-        {/* src=`${API}/${url}/foto/${item._id}}` */}
-        <img src="https://www.tramontina.com/upload/tramon/imagens/CUT/20530616PD001G.jpg"
+        {/* {`${API}/${url}/sUrlImagen/${item._id}`} */}
+
+        <img src={`${item.sUrlImagen}`}
          alt={item.sNombre}
          style={{maxHeight: '100%', maxWidth: '100%'}}
          className="mb-3" />
     </div>
-);
+    );
+}
     
 
 
