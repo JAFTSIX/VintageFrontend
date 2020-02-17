284cd879b631e429e05c0a24cc76481f35a7780e
Rebeca <50533726+Eceber@users.noreply.github.com>
2020-02-16 20:54:09 -0600

    Update apiAdmin.js

--- src/admin/apiAdmin.js
+++ src/admin/apiAdmin.js
@@ -74,3 +74,66 @@ export const eliminarProducto = (productId) => {
         })
 }
 
+
+
+/*---------------------------------RECETA--------------------------------*/
+export const crearReceta = (token,receta) => {
+
+    return fetch(`${API}/Receta`, {
+        method: "POST",
+        headers:{
+            Accept: 'application/json',
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify(receta) //convierte el objeto en json string
+        
+    })
+            
+    .then(response => {
+        return response.json()
+    })
+    .catch(err => {
+        
+        console.log(EvalError)
+    })
+
+}
+
+
+export const modificarReceta = (token,receta) => {
+
+    return fetch(`${API}/Receta/${receta._id}`, {
+        method: "PUT",
+        headers:{
+            Accept: 'application/json',
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify(receta) //convierte el objeto en json string
+        
+    })
+            
+    .then(response => {
+        return response.json()
+    })
+    .catch(err => {
+        
+        console.log(EvalError)
+    })
+}
+
+export const getReceta = (recetaId) => {
+    return fetch(`${API}/Receta/${recetaId}?filter=%7B%0A%20%20%22where%22%3A%20%7B%0A%20%20%20%20%22additionalProp1%22%3A%20%7B%7D%0A%20%20%7D%2C%0A%20%20%22fields%22%3A%20%7B%0A%20%20%20%20%22_id%22%3A%20true%2C%0A%20%20%20%20%22sNombre%22%3A%20true%2C%0A%20%20%20%20%22aEtiqueta%22%3A%20true%2C%0A%20%20%20%20%22dFechaPublicacion%22%3A%20true%2C%0A%20%20%20%20%22sTexto%22%3A%20true%2C%0A%20%20%20%20%22iPrecio%22%3A%20true%2C%0A%20%20%20%20%22sUrlVideo%22%3A%20true%2C%0A%20%20%20%20%22sUrlImagen%22%3A%20true%2C%0A%20%20%20%20%22bActivo%22%3A%20true%0A%20%20%7D%2C%0A%20%20%22offset%22%3A%200%2C%0A%20%20%22limit%22%3A%20100%2C%0A%20%20%22skip%22%3A%200%2C%0A%20%20%22order%22%3A%20%5B%0A%20%20%20%20%22string%22%0A%20%20%5D%0A%7D`, {
+            method: "GET",
+        })
+                
+        .then(response => {
+            return response.json()
+        })
+        .catch(err => {
+            
+            console.log(err);
+        })
+}
+
