07f2ba8d26446eb6a14249e42fc0e336c3a86efd
Rebeca <50533726+Eceber@users.noreply.github.com>
2020-02-16 20:53:58 -0600

    Create MostrarImagenReceta.js

---
+++ src/user/MostrarImagenReceta.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {API} from '../config';
+
+//item = producto
+//url para hacer request al api
+const MostrarImagenReceta = ({item, url}) => {
+    
+    return(
+
+    <div className="productoImagen">
+        {/* {`${API}/${url}/sUrlImagen/${item._id}`} */}
+
+        <img src={`${item.sUrlImagen}`}
+         alt={item.sNombre}
+         style={{maxHeight: '100%', maxWidth: '100%'}}
+         className="mb-3" />
+    </div>
+    );
+}
+    
+
+
+export default MostrarImagenReceta;
