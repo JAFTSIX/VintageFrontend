9385f92fb44d91707f82cb17ae6ba24cc682a812
Rebeca <50533726+Eceber@users.noreply.github.com>
2020-03-07 15:06:25 -0600

    Update ProductoInterfaz.js

--- src/user/ProductoInterfaz.js
+++ src/user/ProductoInterfaz.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import MostrarImagen from './MostrarImagen';
 import {isAutentificacion} from '../autentificacion/index';
+import '../index.css'
 
 const ProductoInterfaz = ({producto}) => {
     return(
