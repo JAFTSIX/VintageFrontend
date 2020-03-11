b6119598f1254b0d5a08553353090f9a1e3ca3d5
yingxz <60948406+yingxz@users.noreply.github.com>
2020-03-10 20:27:16 -0600

    Update Principal.js

--- src/nucleo/Principal.js
+++ src/nucleo/Principal.js
@@ -87,19 +87,19 @@ const Principal = () => {
                 </div>
                 {/* carrousel recetas  */}
                 <div class="row text-center">   
-                    <div class="card ">
+                    <div class="cardP ">
                         <img src="https://okdiario.com/img/recetas/2016/12/29/desayunos-alrededor-del-mundo-2.jpg" className="fotoPerfil"/>
                     </div>
-                    <div class="card">  
+                    <div class="cardP">  
                         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTj4uFKMmJ1sec1f4k1p68ZZLlfSKrupev4BFK6MqKVoa-wB8Qy" className="fotoPerfil"/>
                     </div>
-                    <div class="card">
+                    <div class="cardP">
                         <img src="https://vamosacomerrico.com/wp-content/uploads/2018/02/1300-x-700-23-1-770x540.jpg" className="fotoPerfil"/>
                     </div>
-                    <div class="card">
+                    <div class="cardP">
                         <img src="https://gourmetdemexico.com.mx/wp-content/uploads/2018/06/platillo-salmon-Gravlax.jpg" className="fotoPerfil"/>
                     </div>
-                    <div class="card">  
+                    <div class="cardP">  
                         <img src="https://gastronomiaycia.republica.com/wp-content/photos/postre_turron_chischoco1.jpg" className="fotoPerfil"/>
                     </div>  
                 </div>
@@ -121,19 +121,19 @@ const Principal = () => {
                 </div>
                 {/* carrousel recetas  */}
                 <div class="row text-center">   
-                    <div class="card ">
+                    <div class="cardP ">
                         <img src="https://cdn.shopify.com/s/files/1/0979/9666/products/cuchara-postre-modbaguette_large.jpg?v=1531355541" className="fotoPerfil"/>
                     </div>
-                    <div class="card">  
+                    <div class="cardP">  
                         <img src="https://admin.capris.cr/media/catalog/product/5/7/574297_1.png" className="fotoPerfil"/>
                     </div>
-                    <div class="card">
-                        <img src="https://lh3.googleusercontent.com/proxy/8A3-NNtIzeOPlB3RfTDCxBwHOS8u8y-hSmxq9OxC-uH4cYSzwtMGu6q3kiv-IT6Q3vm-q3lItQgrs6Ef_rwbOmn1f_vOiBKTTY3VR9vtlhoJRCqSuXkQOIjXhniPKmSC1s7aV-uXfxF1vPszn3KWpWEiXGBJQD_2bXKqvUwQthnu4QJMr_aLUHlytE37H1YzyzKajlFjZWautzJTnAtb88bq1KUTAXCh-Vuxw1DyG9AwIYEpUEt9KVwXhLOPHZOuBnBkA5itfuQQNtrgHMO5gd4W81OImeB7bnC1mm1yh0im0OC4iLsbETJONB3gBAEkI45qId27rct1eTy3ov9OGmskAooOYPJBqwjsOHwJrGpo67ygWOvQnwamTVHINxNfj-s-qPU5F4onC2G0MNfvjNejvgJlGwyt5bqRuSEJ" className="fotoPerfil"/>
+                    <div class="cardP">
+                        <img src="https://ferreteriavidri.com/images/items/large/76139.jpg" className="fotoPerfil"/>
                     </div>
-                    <div class="card">
+                    <div class="cardP">
                         <img src="https://www.lecreuset.es/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/o/l/olla-con-tapa-aluminio-antiadherente-le-creuset.jpg" className="fotoPerfil"/>
                     </div>
-                    <div class="card">  
+                    <div class="cardP">  
                         <img src="https://megashoptv.vteximg.com.br/arquivos/ids/157458-900-900/Ollas6.jpg?v=636846320784900000" className="fotoPerfil"/>
                     </div>  
                 </div>
