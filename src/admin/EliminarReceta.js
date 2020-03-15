04d140b08a9b7ff58cd71a3cd257b8ab9969264c
Rebeca <50533726+Eceber@users.noreply.github.com>
2020-03-15 13:01:52 -0600

    Update EliminarReceta.js

--- src/admin/EliminarReceta.js
+++ src/admin/EliminarReceta.js
@@ -4,6 +4,12 @@ import {eliminarReceta} from './apiAdmin';
 import {Redirect} from 'react-router-dom';
 import '../index.css'
 
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 const EliminarReceta= (props) => {
     const [valor, setValor] = useState({          
@@ -13,12 +19,27 @@ const EliminarReceta= (props) => {
 
     const {recetaEliminado, redirect} = valor;
 
+    // controllar el seguro que desea eliminar? 
+    const [open, setOpen] = React.useState(true);
+
+    const handleClose= seguir=>_evento => {
+        console.log(seguir)      
+        if(seguir){
+            eliminar(props.match.params.recetaId)
+        }else{
+            setValor({redirect:true})       
+        }
+        setOpen(false);
+    };
+
+    // llamar al api en admin api 
     const eliminar = recetaId => {
         eliminarReceta(recetaId)
         .then(data=>{
             
             setValor({
-                redirect:true
+                redirect:true,
+                productoEliminado:data
             })
             
         })
@@ -27,7 +48,7 @@ const EliminarReceta= (props) => {
     useEffect(()=>{
         const recetaId = props.match.params.recetaId
         console.log(recetaId)
-        eliminar(recetaId)
+        // eliminar(recetaId)
         
     }, []);
 
@@ -38,24 +59,36 @@ const EliminarReceta= (props) => {
             
     }
 
-    const mostrarFunciona = () => (
-        <div className="alert alert-info" 
-        style={{display: redirect ? '':''}}>
-            <h4>{`Se ha eliminado exitosamente`}</h4>
-        </div>
-    );
 
     return (
-        <Layout titulo="ELIMINAR RECETA" 
-        descripcion="" 
-        className="container-fluid">
-            
-            <div className="row">
-            {mostrarFunciona()}   
-                   {redireccionarUsuario()}
+        <div className="row">
+
+            <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description">
+    
+            <DialogTitle id="alert-dialog-title">{`Eliminar`}</DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+               ¿Esta seguro que deseas eliminar este producto?
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose(false)} color="primary">
+                No
+              </Button>
+              <Button onClick={handleClose(true)} color="primary" autoFocus>
+                Seguro
+              </Button>
+            </DialogActions>
+          </Dialog> 
+
+         
+          {redireccionarUsuario()}
                              
             </div>
-        </Layout>
     );
 }
 
