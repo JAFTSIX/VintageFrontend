4b70d3d23b768fc5dbb1481a9db728fd991f5c71
Rebeca <50533726+Eceber@users.noreply.github.com>
2020-03-28 23:17:20 -0600

    Update RecetaInterfaz.js

--- src/user/RecetaInterfaz.js
+++ src/user/RecetaInterfaz.js
@@ -36,14 +36,14 @@ const RecetaInterfaz = ({receta}) => {
                         <Fragment>
                         {/* // aqui se pasa el id del producto en el url para actualizar */}
                         <Link to={`/Receta/${receta._id}/?filter=%7B%0A%20%20%22where%22%3A%20%7B%0A%20%20%20%20%22additionalProp1%22%3A%20%7B%7D%0A%20%20%7D%2C%0A%20%20%22fields%22%3A%20%7B%0A%20%20%20%20%22_id%22%3A%20true%2C%0A%20%20%20%20%22sNombre%22%3A%20true%2C%0A%20%20%20%20%22aEtiqueta%22%3A%20true%2C%0A%20%20%20%20%22dFechaPublicacion%22%3A%20true%2C%0A%20%20%20%20%22sTexto%22%3A%20true%2C%0A%20%20%20%20%22iPrecio%22%3A%20true%2C%0A%20%20%20%20%22sUrlVideo%22%3A%20true%2C%0A%20%20%20%20%22sUrlImagen%22%3A%20true%2C%0A%20%20%20%20%22bActivo%22%3A%20true%0A%20%20%7D%2C%0A%20%20%22offset%22%3A%200%2C%0A%20%20%22limit%22%3A%20100%2C%0A%20%20%22skip%22%3A%200%2C%0A%20%20%22order%22%3A%20%5B%0A%20%20%20%20%22string%22%0A%20%20%5D%0A%7D`}>
-                            <button className="btn customBtn btnPinkFuerte
+                            <button className="btn customBtn btnPinkFuerte btnPink
                             mt-2 mb-2 agregarPadding mr-2 text-light">
                                 Modificar Receta
                             </button>
                         </Link>
 
                         <Link to={`Receta/Eliminar/${receta._id}`}>
-                            <button className="btn customBtn btnPinkFuerte text-light
+                            <button className="btn customBtn btnPinkFuerte text-light btnPink
                             mt-2 mb-2 agregarPadding mr-2" data-toggle="modal" data-target="#exampleModal">
                                 Eliminar Receta
                             </button>
