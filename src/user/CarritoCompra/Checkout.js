54c738ce6880e260c8a58bf7d614c438ca92efb5
Rebeca <50533726+Eceber@users.noreply.github.com>
2020-03-28 20:47:40 -0600

    Update Checkout.js

--- src/user/CarritoCompra/Checkout.js
+++ src/user/CarritoCompra/Checkout.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import {getProductosLocalStorage, actualizarCantidad, eliminarProductoCarrito} from './carritoHelper';
 import '../../index.css';
 import './carrito.css';
+import { PayPalButton } from "react-paypal-button-v2";
+
 
 const Checkout = ({products}) => {
     {/* calcular el total de carrito de compra  */}
@@ -27,12 +29,35 @@ const Checkout = ({products}) => {
         return total;
     }
 
+
+
     return <div>
                 {/* calcular total de producto  */}
                 <h1>Total de Productos: {getTotalProductos()}</h1>
                 {/* calcular el total de carrito de compra  */}
                 <h1>Total: ₡{getTotal()}</h1>
-            </div>
+                {/* <PayPalButton
+
+                        amount={getTotal()}
+                        // shippingPreference="NO_SHIPPING" // default is "GET_FROM_FILE"
+                        onSuccess={(details, data) => {
+                        alert("Transacción Completada");
+                
+                        // OPTIONAL: Call your server to save the transaction
+                        return fetch("/paypal-transaction-complete", {
+                            method: "post",
+                            body: JSON.stringify({
+                            orderID: data.orderID
+                            })
+                        });
+                        }}
+                    /> */}
+
+
+
+ 
+
+        </div>
 }
 
 export default Checkout;
