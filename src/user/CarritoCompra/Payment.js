5204494718b3fcceea974c6a86f8a7cb7b69fb7e
Rebeca <50533726+Eceber@users.noreply.github.com>
2020-03-28 20:47:42 -0600

    Create Payment.js

---
+++ src/user/CarritoCompra/Payment.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect, useRef } from 'react';
+import '../../index.css';
+import './carrito.css';
+
+const Carrito = () => {
+
+    const [paidFor, setPaidFor] = useState(false);
+    const [loaded, setLoaded] = useState(false);
+
+    let paypalRef = useRef();
+
+}
+
+export default Carrito;
