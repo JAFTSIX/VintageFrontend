6d694a323b2e2ee444ca49acde8132ba00fd02f5
Rebeca <50533726+Eceber@users.noreply.github.com>
2020-03-29 15:35:27 -0600

    Update listar.js

--- src/admin/support/Historial/listar.js
+++ src/admin/support/Historial/listar.js
@@ -63,7 +63,7 @@ const Historial = () => {
     return (
         <Layout titulo="Historial" 
         jumbotron="jumboEstatico" 
-        image="https://scontent.fsyq3-1.fna.fbcdn.net/v/t1.0-9/p960x960/67816237_3035758673117759_5101590943354585088_o.jpg?_nc_cat=111&_nc_sid=9e2e56&_nc_ohc=G4PV26zKDcYAX-Otv_W&_nc_ht=scontent.fsyq3-1.fna&_nc_tp=6&oh=82da2879ed4e28ce3f551e3262cbd86f&oe=5EA7151C"
+        image="https://scontent.fsyq3-1.fna.fbcdn.net/v/t1.0-0/p640x640/67296179_3012585752101718_1249727829780725760_o.jpg?_nc_cat=104&_nc_sid=110474&_nc_ohc=3EWxP05wr1wAX9Ad7M9&_nc_ht=scontent.fsyq3-1.fna&_nc_tp=6&oh=a0f2746059951017d12d13b5a9990112&oe=5EA50410"
         descripcion="Chef Selenia Mendez" 
         className="container-fluid">
         
