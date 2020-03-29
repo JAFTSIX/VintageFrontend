8fd9f764dc766c52096b8d96e7df6741db9226b1
JAFTSIX <42688559+JAFTSIX@users.noreply.github.com>
2020-03-29 17:32:07 -0600

    video route creado

---
+++ src/autentificacion/VideoRoute.js
@@ -0,0 +1,24 @@
+//la vista privada, para verla, primero necesita loguearse
+import React, { Component } from 'react';
+import {Route, Redirect} from 'react-router-dom'; //documentacion en google react router dom
+import {isAutentificacion} from './index'; //por ahora todavia no se ha hecho
+import '../index.css'
+
+
+//parametro: component de react y el resto del route
+const VideoRoute = ({component: Component, ...rest}) => (
+    //si es es autentico, retorna el componente con props, 
+    //en caso contrario redirecciona a log in
+    <Route  {...rest} render={props => 
+        //solo existe en el local storage, y el correo es rebe y contrase;a 123 se puede ingresar como admin
+        isAutentificacion() 
+        && isAutentificacion().cliente.bAdmin ? (
+        //si sí se loguea:
+        <Component {...props}/>
+    ) : (
+        //si no se loguea se redirecciona
+        <Redirect to={{pathname: '/signIn', state:{from: props.location}}}/>
+    )}/>
+);
+
+export default VideoRoute;
