12e6d5ee7237651c38c0ff87ccf4b1df9c84c71e
Rebeca <50533726+Eceber@users.noreply.github.com>
2020-03-29 19:18:19 -0600

    Update RecetaDetalle.js

--- src/user/RecetaDetalle.js
+++ src/user/RecetaDetalle.js
@@ -59,52 +59,58 @@ const RecetaDetalle = (props) => {
         );  
     };
 
+    const botones = () =>{
+        return(
+            
+            <div className="row">   
+
+            {/* botones admin  */}
+             <div className="btnAdminReceta">
+             {isAutentificacion() 
+             && isAutentificacion().cliente.bAdmin && (
+                 <Fragment>
+                     
+                     {/* // aqui se pasa el id de la receta en el url para actualizar */}
+                     <Link to={`/Receta/${receta._id}/?filter=%7B%0A%20%20%22where%22%3A%20%7B%0A%20%20%20%20%22additionalProp1%22%3A%20%7B%7D%0A%20%20%7D%2C%0A%20%20%22fields%22%3A%20%7B%0A%20%20%20%20%22_id%22%3A%20true%2C%0A%20%20%20%20%22sNombre%22%3A%20true%2C%0A%20%20%20%20%22aEtiqueta%22%3A%20true%2C%0A%20%20%20%20%22dFechaPublicacion%22%3A%20true%2C%0A%20%20%20%20%22sTexto%22%3A%20true%2C%0A%20%20%20%20%22iPrecio%22%3A%20true%2C%0A%20%20%20%20%22sUrlVideo%22%3A%20true%2C%0A%20%20%20%20%22sUrlImagen%22%3A%20true%2C%0A%20%20%20%20%22bActivo%22%3A%20true%0A%20%20%7D%2C%0A%20%20%22offset%22%3A%200%2C%0A%20%20%22limit%22%3A%20100%2C%0A%20%20%22skip%22%3A%200%2C%0A%20%20%22order%22%3A%20%5B%0A%20%20%20%20%22string%22%0A%20%20%5D%0A%7D`}>
+                         <button className="btn btn-outline-primary mr-2">
+                                 Modificar Receta
+                         </button>
+                     </Link>
+ 
+                     <Link to={`../Eliminar/${receta._id}`}>
+                         <button className="btn btn-outline-primary">
+                                 Eliminar Receta
+                         </button>  
+                     
+                     </Link>
+ 
+                 </Fragment>
+             )}
+                 {/* btn regresar a receta general  */}
+                 <Link to={`/Receta`}>
+                         <button className="btn btn-outline-primary">
+                                 Atras
+                         </button>
+                     
+                 </Link>
+              </div>
+              {/* imagen prodcuto  */} 
+              <img src={receta.sUrlImagen} height="110px" width="110px" className="imagenReceta"></img>
+              </div>
+        );
+    }
+
     return(
         
         <div className="mt10 mx-5 container-fluid   Content">
             <div className="msgStatic">{mostrarFunciona()}</div>
             <Menu />
-            <h1 className="text-capitalize">{receta.sNombre}</h1>
-            <h4 className="mb-5">Detalle de Receta</h4>
-            {/* imagen prodcuto  */} 
-            
-            <div className="row">   
-                <img src={receta.sUrlImagen} height="110px" width="110px" className="imagenReceta"></img>
+            <div className="row">
+                <h1 className="text-capitalize mb-5 col-lg-5">{receta.sNombre}</h1>
+                <div className="col-lg-7">
+                    {botones()}
+                </div>
             </div>
-
-            {/* botones admin  */}
-            <div className="btnAdminReceta">
-            {isAutentificacion() 
-            && isAutentificacion().cliente.bAdmin && (
-                <Fragment>
-                    
-                    {/* // aqui se pasa el id de la receta en el url para actualizar */}
-                    <Link to={`/Receta/${receta._id}/?filter=%7B%0A%20%20%22where%22%3A%20%7B%0A%20%20%20%20%22additionalProp1%22%3A%20%7B%7D%0A%20%20%7D%2C%0A%20%20%22fields%22%3A%20%7B%0A%20%20%20%20%22_id%22%3A%20true%2C%0A%20%20%20%20%22sNombre%22%3A%20true%2C%0A%20%20%20%20%22aEtiqueta%22%3A%20true%2C%0A%20%20%20%20%22dFechaPublicacion%22%3A%20true%2C%0A%20%20%20%20%22sTexto%22%3A%20true%2C%0A%20%20%20%20%22iPrecio%22%3A%20true%2C%0A%20%20%20%20%22sUrlVideo%22%3A%20true%2C%0A%20%20%20%20%22sUrlImagen%22%3A%20true%2C%0A%20%20%20%20%22bActivo%22%3A%20true%0A%20%20%7D%2C%0A%20%20%22offset%22%3A%200%2C%0A%20%20%22limit%22%3A%20100%2C%0A%20%20%22skip%22%3A%200%2C%0A%20%20%22order%22%3A%20%5B%0A%20%20%20%20%22string%22%0A%20%20%5D%0A%7D`}>
-                        <button className="btn btn-outline-primary 
-                            agregarPadding mr-2">
-                                Modificar Receta
-                        </button>
-                    </Link>
-
-                    <Link to={`../Eliminar/${receta._id}`}>
-                        <button className="btn btn-outline-primary
-                            agregarPadding ml-2" data-toggle="modal" data-target="#exampleModal">
-                                Eliminar Receta
-                        </button>
-                    
-                    </Link>
-
-                </Fragment>
-            )}
-                {/* btn regresar a receta general  */}
-                <Link to={`/Receta`}>
-                        <button className="btn btn-outline-primary
-                            agregarPadding ml-3" data-toggle="modal" data-target="#exampleModal">
-                                Atras
-                        </button>
-                    
-                </Link>
-             </div>
 
             {/* contenido  */}
             <div className="row mt-1 Content">
