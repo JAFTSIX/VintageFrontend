e572553aaa2cd2453efa9139c1fdc4f29180011a
Rebeca <50533726+Eceber@users.noreply.github.com>
2020-04-07 01:01:35 -0600

    Update ModificarPerfil.js

--- src/user/ModificarPerfil.js
+++ src/user/ModificarPerfil.js
@@ -1,7 +1,7 @@
 import React, { Fragment,useState,useEffect } from 'react';
 import Layout from '../nucleo/Layout';
 import {readPerfil, modificarPerfil,modificarPerfilLocal, isAutentificacion} from '../autentificacion';
-import {Link} from 'react-router-dom';
+import {Link, Redirect} from 'react-router-dom';
 import '../index.css';  
 import '../css.css';
  
@@ -75,13 +75,19 @@ const ModificarPerfil = ({match}) => {
                     sCorreo: data.sCorreo,
                     dNacimiento: data.dNacimiento,
                     oDireccion: {
-                        provincia:data.oDireccion.provincia,
-                        canton:data.oDireccion.canton,
-                        direccion1:data.oDireccion.direccion1,
-                        direccion2:data.oDireccion.direccion2,
-                        codPostal:data.oDireccion.codPostal,
-                        telefono:data.oDireccion.telefono,
-                    }
+                        provincia:dProvincia,
+                        canton:data.dCanton,
+                        direccion1:data.dDireccion1,
+                        direccion2:data.dDireccion2,
+                        codPostal:data.dCodPostal,
+                        telefono:data.dTelefono,
+                    },
+                    dProvincia:data.oDireccion.provincia,
+                    dCanton:data.oDireccion.canton,
+                    dDireccion1:data.oDireccion.direccion1,
+                    dDireccion2:data.oDireccion.direccion2,
+                    dCodPostal:data.oDireccion.codPostal,
+                    dTelefono:data.oDireccion.telefono,
                 })
             }
         });
@@ -91,8 +97,80 @@ const ModificarPerfil = ({match}) => {
         init(match.params.perfilId);
     }, []);
 
-    const handleChange = (e) =>{
-        //
+    const handleChange = sNombre=> e =>{
+        
+            
+            console.log(values);
+        setValues({...values, error: false, [sNombre]: e.target.value});
+            oDireccion.provincia=dProvincia;
+            oDireccion.canton=dCanton;
+            oDireccion.direccion1=dDireccion1;
+            oDireccion.direccion2=dDireccion2;
+            oDireccion.codPostal=dCodPostal;
+            oDireccion.telefono=dTelefono;
+        
+    }
+
+    oDireccion.provincia=dProvincia;
+            oDireccion.canton=dCanton;
+            oDireccion.direccion1=dDireccion1;
+            oDireccion.direccion2=dDireccion2;
+            oDireccion.codPostal=dCodPostal;
+            oDireccion.telefono=dTelefono;
+    
+
+    const clickSubmit = (e) => {
+
+            
+ 
+        e.preventDefault();
+        modificarPerfil(match.params.perfilId, token, {
+            sNombre,
+            sApellido,
+            sContrasena,
+            sCorreo,
+            dNacimiento,
+            oDireccion
+        }).then((data={error:{message:'Correcto'}} ) =>{
+            if('error' in data ){
+                setValues({...values, error:data.error.message, funciona: false});
+
+            }else{
+                modificarPerfilLocal(data, ()=>{
+                    setValues({
+                        ...values,
+                        _id: data._id,
+                        sNombre: data.sNombre,
+                        sApellido: data.sApellido,
+                        sContrasena: data.sContrasena,
+                        password: "",
+                        sCorreo: data.sCorreo,
+                        dNacimiento: data.dNacimiento,
+                        oDireccion: data.oDireccion,
+                        aRecetas: data.aRecetas,
+                        aFavoritos: data.aFavoritos,    
+                        bActivo: data.bActivo,
+                        funciona:true
+                    })
+                });
+            }
+            console.log(values);
+        });
+    }
+
+    const mostrarError = () => (
+        <div className="alert alert-danger" 
+        style={{display: error ? '' : 'none'}}>
+            {error}
+           
+        </div>
+        
+    );
+
+    const redireccionarUsuario = (funciona) =>{
+        if(funciona){
+            return <Redirect to="/perfil"/>
+        }
     }
 
     const modificarPerfilForm = (
@@ -102,99 +180,108 @@ const ModificarPerfil = ({match}) => {
         password,
         sCorreo,
         dNacimiento,
-        provincia,
-        canton,
-        direccion1,
-        direccion2,
-        codPostal,
-        telefono) => (
+        dProvincia,
+        dCanton,
+        dDireccion1,
+        dDireccion2,
+        dCodPostal,
+        dTelefono) => (
         <form>
-            <div className="form-group mt-30">
-                <label className="text-muted">
-                    Nombre 
-                </label>
-                <input name="sNombre" onChange={handleChange('sNombre')}  type="text" 
-                    className="form-control" value={sNombre}/>
+            <div className="row">
+            <div className="col-lg-6">
+                <div className="form-group mt-30">
+                    <label className="text-muted">
+                        Nombre 
+                    </label>
+                    <input name="sNombre" onChange={handleChange('sNombre')}  type="text" 
+                        className="form-control" value={sNombre}/>
+                </div>
+                <div className="form-group mt-30">
+                    <label className="text-muted">
+                        Apellido 
+                    </label>
+                    <input name="sApellido" onChange={handleChange('sApellido')}  type="text" 
+                        className="form-control" value={sApellido}/>
+                </div>
+                <div className="form-group mt-30">
+                    <label className="text-muted">
+                        Contraseña 
+                    </label>
+                    <input name="sContrasena" onChange={handleChange('sContrasena')}  type="password" 
+                        className="form-control" value={sContrasena}/>
+                </div>
+                <div className="form-group mt-30">
+                    <label className="text-muted">
+                        Confirmar Contraseña 
+                    </label>
+                    <input name="password"   type="password" 
+                        className="form-control"/>
+                </div>
+                <div className="form-group mt-30">
+                    <label className="text-muted">
+                        Correo Electronico 
+                    </label>
+                    <input name="sCorreo" onChange={handleChange('sCorreo')}  type="email" 
+                        className="form-control" value={sCorreo}/>
+                </div>
+                <div className="form-group mt-30">
+                    <label className="text-muted">
+                        Fecha Nacimiento
+                    </label>
+                    <input name="sCorreo" onChange={handleChange('dNacimiento')}  type="text" 
+                        className="form-control" value={dNacimiento}/>
+                </div>
             </div>
-            <div className="form-group mt-30">
-                <label className="text-muted">
-                    Apellido 
-                </label>
-                <input name="sApellido" onChange={handleChange('sApellido')}  type="text" 
-                    className="form-control" value={sApellido}/>
+            <div className="col-lg-6">
+                <div className="form-group mt-30">
+                    <label className="text-muted">
+                        Provincia 
+                    </label>
+                    <input name="dProvincia" onChange={handleChange('dProvincia')}  type="text" 
+                        className="form-control" value={dProvincia}/>
+                </div>
+                <div className="form-group mt-30">
+                    <label className="text-muted">
+                        Cantón 
+                    </label>
+                    <input name="dCanton" onChange={handleChange('dCanton')}  type="text" 
+                        className="form-control" value={dCanton}/>
+                </div>
+                <div className="form-group mt-30">
+                    <label className="text-muted">
+                        Dirección 1 
+                    </label>
+                    <input name="dDireccion1" onChange={handleChange('dDireccion1')}  type="text" 
+                        className="form-control" value={dDireccion1}/>
+                </div>
+                <div className="form-group mt-30">
+                    <label className="text-muted">
+                        Dirección 2 
+                    </label>
+                    <input name="dDireccion2" onChange={handleChange('dDireccion2')}  type="text" 
+                        className="form-control" value={dDireccion2}/>
+                </div>
+                <div className="form-group mt-30">
+                    <label className="text-muted">
+                        Código Postal 
+                    </label>
+                    <input name="dCodPostal" onChange={handleChange('dCodPostal')}  type="text" 
+                        className="form-control" value={dCodPostal}/>
+                </div>
+                <div className="form-group mt-30">
+                    <label className="text-muted">
+                        Teléfono 
+                    </label>    
+                    <input name="dTelefono" onChange={handleChange('dTelefono')}  type="text" 
+                        className="form-control" value={dTelefono}/>
+                </div>
             </div>
-            <div className="form-group mt-30">
-                <label className="text-muted">
-                    Contraseña 
-                </label>
-                <input name="sContrasena" onChange={handleChange('sContrasena')}  type="text" 
-                    className="form-control" value={sContrasena}/>
+            <div className="btnCentral">
+            <button   onClick={clickSubmit}   className="btn btn-outline-primary btnCentral">
+                Modificar Cuenta
+            </button>
             </div>
-            <div className="form-group mt-30">
-                <label className="text-muted">
-                    Confirmar Contraseña 
-                </label>
-                <input name="dProvincia"   type="text" 
-                    className="form-control" value={}/>
             </div>
-            <div className="form-group mt-30">
-                <label className="text-muted">
-                    Correo Electronico 
-                </label>
-                <input name="sCorreo" onChange={handleChange('sCorreo')}  type="text" 
-                    className="form-control" value={sCorreo}/>
-            </div>
-            <div className="form-group mt-30">
-                <label className="text-muted">
-                    Fecha Nacimiento
-                </label>
-                <input name="sCorreo" onChange={handleChange('dNacimiento')}  type="text" 
-                    className="form-control" value={dNacimiento}/>
-            </div>
-
-            <div className="form-group mt-30">
-                <label className="text-muted">
-                    Provincia 
-                </label>
-                <input name="oDireccion.provincia" onChange={handleChange('oDireccion.provincia')}  type="text" 
-                    className="form-control" value={oDireccion.provincia}/>
-            </div>
-            <div className="form-group mt-30">
-                <label className="text-muted">
-                    Cantón 
-                </label>
-                <input name="oDireccion.canton" onChange={handleChange('oDireccion.canton')}  type="text" 
-                    className="form-control" value={oDireccion.canton}/>
-            </div>
-            <div className="form-group mt-30">
-                <label className="text-muted">
-                    Dirección 1 
-                </label>
-                <input name="oDireccion.direccion1" onChange={handleChange('oDireccion.direccion1')}  type="text" 
-                    className="form-control" value={oDireccion.direccion1}/>
-            </div>
-            <div className="form-group mt-30">
-                <label className="text-muted">
-                    Dirección 2 
-                </label>
-                <input name="oDireccion.direccion2" onChange={handleChange('oDireccion.direccion2')}  type="text" 
-                    className="form-control" value={oDireccion.direccion2}/>
-            </div>
-            <div className="form-group mt-30">
-                <label className="text-muted">
-                    Código Postal 
-                </label>
-                <input name="oDireccion.codPostal" onChange={handleChange('oDireccion.codPostal')}  type="text" 
-                    className="form-control" value={oDireccion.codPostal}/>
-            </div>
-            <div className="form-group mt-30">
-                <label className="text-muted">
-                    Teléfono 
-                </label>    
-                <input name="oDireccion.telefono" onChange={handleChange('oDireccion.telefono')}  type="text" 
-                    className="form-control" value={oDireccion.telefono}/>
-            </div>
-
         </form>
     );
 
@@ -202,9 +289,10 @@ const ModificarPerfil = ({match}) => {
         <Layout jumbotron="jumbotronMovimiento"
         image="https://wallroom.io/img/2560x1440/bg-e4f1f74.jpg"
         titulo="Modificar Perfil" 
-        // descripcion={`${sNombre} ${sApellido}`} 
-        className="container-fluid mt-5">
-
+        descripcion={`${sNombre} ${sApellido}`} 
+        className="container mt-5">
+            {mostrarError()}
+            {redireccionarUsuario(funciona)}
             {modificarPerfilForm(
                 sNombre,
                 sApellido,
@@ -212,13 +300,15 @@ const ModificarPerfil = ({match}) => {
                 password,
                 sCorreo,
                 dNacimiento,
-                oDireccion.provincia,
-                oDireccion.canton,
-                oDireccion.direccion1,
-                oDireccion.direccion2,
-                oDireccion.codPostal,
-                oDireccion.telefono,
+                dProvincia,
+                dCanton,
+                dDireccion1,
+                dDireccion2,
+                dCodPostal,
+                dTelefono
             )}
+            
+            
 
         </Layout>
 
