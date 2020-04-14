1bb1834b55e66728c4c772b7f07203aaae72a8ee
Rebeca <50533726+Eceber@users.noreply.github.com>
2020-04-13 22:03:34 -0600

    Update RecetaDetalle.js

--- src/user/RecetaDetalle.js
+++ src/user/RecetaDetalle.js
@@ -48,16 +48,6 @@ const RecetaDetalle = (props) => {
 
 
 
-
-
-
-
-
-
-
-
-
-
     const isVer = (recetaId,ruta) => {
         // funcion ubicado en apiReceta 
         ///Cliente/Ver/{id}
@@ -170,13 +160,13 @@ const RecetaDetalle = (props) => {
                      
                      {/* // aqui se pasa el id de la receta en el url para actualizar */}
                      <Link to={`/Receta/${receta._id}/?filter=%7B%0A%20%20%22where%22%3A%20%7B%0A%20%20%20%20%22additionalProp1%22%3A%20%7B%7D%0A%20%20%7D%2C%0A%20%20%22fields%22%3A%20%7B%0A%20%20%20%20%22_id%22%3A%20true%2C%0A%20%20%20%20%22sNombre%22%3A%20true%2C%0A%20%20%20%20%22aEtiqueta%22%3A%20true%2C%0A%20%20%20%20%22dFechaPublicacion%22%3A%20true%2C%0A%20%20%20%20%22sTexto%22%3A%20true%2C%0A%20%20%20%20%22iPrecio%22%3A%20true%2C%0A%20%20%20%20%22sUrlVideo%22%3A%20true%2C%0A%20%20%20%20%22sUrlImagen%22%3A%20true%2C%0A%20%20%20%20%22bActivo%22%3A%20true%0A%20%20%7D%2C%0A%20%20%22offset%22%3A%200%2C%0A%20%20%22limit%22%3A%20100%2C%0A%20%20%22skip%22%3A%200%2C%0A%20%20%22order%22%3A%20%5B%0A%20%20%20%20%22string%22%0A%20%20%5D%0A%7D`}>
-                         <button className="btn btn-outline-primary borderRadio0 border-right-0">
+                         <button className="btn btn-outline-primary borderRadio0">
                                  Modificar Receta
                          </button>
                      </Link>
  
                      <Link to={`../Eliminar/${receta._id}`}>
-                         <button className="btn btn-outline-primary borderRadio0 border-right-0">
+                         <button className="btn btn-outline-primary borderRadio0">
                                  Eliminar Receta
                          </button>  
 
@@ -206,7 +196,7 @@ const RecetaDetalle = (props) => {
 
     return(
         
-        <div className="mt10 mx-5 container-fluid   Content">
+        <div className="mt10 container-fluid   Content">
             <div className="msgStatic"></div>
             <Menu />
             <div className="row">
@@ -214,20 +204,20 @@ const RecetaDetalle = (props) => {
             {mostrarError ()}{mostrarFunciona()}
             
        
-                <div className="col-lg-6 col-md-12 recetaDetalleTitulo">
+                <div className="col-lg-6 col-md-12 col-12 recetaDetalleTitulo">
                     <h1 className="text-capitalize mb-5 mr-5">{receta.sNombre}</h1>
                     {/* imagen prodcuto  */} 
                     <img src={receta.sUrlImagen} height="70px" width="110px" className="imagenReceta"></img>
                 </div>
-                <div className="col-lg-6 col-md-12 recetaDetalleBtn">
+                <div className="col-lg-6 col-md-12 col-12 recetaDetalleBtn">
                     {botones()} 
                 </div>
-            </div>
+            </div>  
 
             {/* contenido  */}
             <div className="row mt-1 Content">
             {ver && 
-                <div className="col-lg-6 col-md-11 fix">     
+                <div className="col-lg-6 col-md-12 fix">     
                 
                     <iframe className="video" src={receta.sUrlVideo} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>  
@@ -235,13 +225,13 @@ const RecetaDetalle = (props) => {
             }
 
             {!ver&&(
-                <div className="col-lg-6 col-md-11 fix">     
+                <div className="col-lg-6 col-md-12 fix">     
                    
                     <iframe className="video" src={receta.sUrlVideoTrailer} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>  )
 
             }
-                 <div className="col-lg-5 col-md-11 scroll mr-1">     
+                 <div className="col-lg-6 col-md-12 scroll">     
             
             
 
