a167a269435fd62ee2ef79e7b0fc7e59fb12c19c
Rebeca <50533726+Eceber@users.noreply.github.com>
2020-07-21 18:31:38 -0600

    Update listar.js

--- src/admin/support/Historial/listar.js
+++ src/admin/support/Historial/listar.js
@@ -62,7 +62,7 @@ const Historial = () => {
     return (
         <Layout titulo="Historial" 
         jumbotron="jumboEstatico" 
-        image="https://scontent.fsyq3-1.fna.fbcdn.net/v/t1.0-0/p640x640/67296179_3012585752101718_1249727829780725760_o.jpg?_nc_cat=104&_nc_sid=110474&_nc_ohc=3EWxP05wr1wAX9Ad7M9&_nc_ht=scontent.fsyq3-1.fna&_nc_tp=6&oh=a0f2746059951017d12d13b5a9990112&oe=5EA50410"
+        image="https://sc01.alicdn.com/kf/HTB1bPABKVXXXXagXFXXq6xXFXXXm.jpg"
         descripcion="Chef Selenia Mendez" 
         className="container-fluid">
         
