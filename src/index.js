d7952c6b97ddc2445e1401d51c3a0edee43cc376
JAFTSIX <42688559+JAFTSIX@users.noreply.github.com>
2020-08-09 20:16:00 -0600

    deploy test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes';
 import './index.css'
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Routes />, document.getElementById('root'));
 
+registerServiceWorker();
 
