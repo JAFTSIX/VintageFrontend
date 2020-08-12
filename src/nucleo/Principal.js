014c8369b978fc8b4065ed649a7eb45668b579a8
Rebeca <50533726+Eceber@users.noreply.github.com>
2020-08-11 21:21:51 -0600

    Update Principal.js

--- src/nucleo/Principal.js
+++ src/nucleo/Principal.js
@@ -25,11 +25,6 @@ const Principal = () => {
             </div>
             {/* carrousel  */}
             <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol>
                 <div class="carousel-inner">    
                     <div class="carousel-item active"> 
                         <div className="overlay"></div> 
@@ -40,33 +35,7 @@ const Principal = () => {
                                     <h3>Chef Selenia Mendez</h3>
                                 </div>
                     </div>
-                    <div class="carousel-item">
-                    <div className="overlay"></div>   
-                    <div className="imgCarrousel2"></div> 
-                    
-                    <img class="d-block w-100" src={banner2} alt="Second slide"/>
-                        <div class="carousel-caption  d-md-block">
-                            <h1 className="sizeB">MEJORES RECETAS</h1>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                    <div className="overlay"></div>
-                    <div className="imgCarrousel3"></div>
-                    <img class="d-block w-100" src={banner3} alt="Third slide"/>
-                        <div class="carousel-caption d-md-block">
-                            <h1 className="sizeB">MEJORES SABORES</h1>
-                        </div>
-
-                    </div>
                 </div>
-                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
             </div>
 
             <div class="container">
